3fe567683f7ba3743eb0e62b6fda9e123d06a25a
JaredGran <jared.carper@gmail.com>
2022-06-10 14:45:26 -0700

    stuck. taking 10

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {
-  $("form#formOne").submit(function(event) {
+  $("form#formTwo").submit(function(event) {
     event.preventDefault()
   
-    const array = [0,1,2,3,4,5];
-    array.forEach(function(number) { 
-      console.log(number * 2);
-    });
+  
      function beepBoop(){
-     userImput = $("#userImput".val())
+     const userImput = $("#userImput".val())
       for (let i = 0; i <= userImput; i ++); 
+    if (i)
+      console.log(beepBoop())
     } 
   beepBoop;
   })
 });
-$("#text-passage").val();
