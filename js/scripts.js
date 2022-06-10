12811ce893ab6d31d9e3968bfb028a57aee6b857
JaredGran <jared.carper@gmail.com>
2022-06-10 13:43:32 -0700

    taking 10 min break

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,15 @@ $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault()
   
-    beepBoop(5);
-    if {
-    
-  
+    const array = [0,1,2,3,4,5];
+    array.forEach(function(number) { 
+      console.log(number * 2);
+    });
+     function beepBoop(){
+     userImput = $("#userImput".val())
+      for (let i = 0; i <= userImput; i ++); 
+    } 
+  beepBoop;
   })
 });
+$("#text-passage").val();
